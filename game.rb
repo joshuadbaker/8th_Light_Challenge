a94615dcbb5ec92ffbee151c4bdb685e48a7680f
joshuadbaker <jbakercoder@gmail.com>
2015-11-09 13:36:12 -0500

    added gameboard model, still need to finish methods

--- game.rb
+++ game.rb
@@ -1,15 +1,17 @@
 require 'pry'
 require './human.rb'
+require './computer.rb'
+require './gameboard.rb'
 class Game
-  attr_accessor :board, :turns, :player_1, :player_2
+  attr_accessor :player_1, :player_2, :new_gameboard
   def initialize()
-    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    @turns = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
     @player_1 = player_1
     @player_2 = player_2
+    @new_gameboard = new_gameboard
   end
 
   def create_new_game
+    @new_gameboard = Gameboard.new()
     puts "Hello..."
     # sleep(2)
     puts "...Dave"
@@ -22,22 +24,10 @@ class Game
     if response == "s" || response == "q"
       until response == "q"
         if response == "s"
-          puts "Select number for version of game:\n 1. Person vs. Person\n 2. Person vs. Computer\n 3. Computer vs. Computer"
-          game_version = gets.chomp
-          game_version = game_version.to_i
-          if game_version == 1
-            puts "You chose person vs person!"
-            person_vs_person
-          elsif game_version == 2
-            puts "person vs Hal"
-          elsif game_version == 3
-            puts "Hal vs Skynet"
-          else 
-            abort("Thank you for playing!")
-          end    
+          select_version     
         end
         coin_toss
-        start_game 
+        @new_gameboard.take_turn(@new_gameboard.board, @new_gameboard.turns, @player_1, @player_2) 
       end
     else 
       puts "Please enter a valid command with the first letter."
@@ -46,15 +36,32 @@ class Game
     end
   end
 
+  def select_version
+    puts "Select number for version of game:\n 1. Person vs. Person\n 2. Person vs. Computer\n 3. Computer vs. Computer"
+    game_version = gets.chomp
+    game_version = game_version.to_i
+    if game_version == 1
+      puts "You chose person vs person!"
+      person_vs_person
+    elsif game_version == 2
+      puts "You chose person vs computer, good luck against Hal!"
+      person_vs_computer
+    elsif game_version == 3
+      puts "Hal vs Skynet"
+    else 
+      abort("Thank you for playing!")
+    end
+  end
+
   def person_vs_person
     puts "Please enter the name for player 1."
     name_1 = gets.chomp
     puts 'Please enter a single character as your game symbol(The game will default to "X")'
+    # binding.pry
     symbol_1 = gets.chomp
     @player_1 = Human.new(name_1, symbol_1)
-    # create_person(@player_1, name_1, symbol_1)
     sleep(1)
-    puts "Thank you, you name is #{@player_1.name} and you game symbol is #{@player_1.game_symbol}"
+    puts "Thank you, your name is #{@player_1.name} and your game symbol is #{@player_1.game_symbol}"
     
     puts "Please enter the name for player 2."
     name_2 = gets.chomp
@@ -62,9 +69,23 @@ class Game
     symbol_2 = gets.chomp
     @player_2 = Human.new(name_2, symbol_2)
     sleep(1)
-    puts "Thank you, you name is #{@player_2.name} and you game symbol is #{@player_2.game_symbol}"
+    puts "Thank you, your name is #{@player_2.name} and your game symbol is #{@player_2.game_symbol}"
   end
 
+  def person_vs_computer
+    puts "Please enter the name for player 1."
+    name_1 = gets.chomp
+    puts 'Please enter a single character as your game symbol(The game will default to "X")'
+    symbol_1 = gets.chomp
+    @player_1 = Human.new(name_1, symbol_1)
+    # create_person(@player_1, name_1, symbol_1)
+    sleep(1)
+    @player_2 = Computer.new(name, game_symbol)
+    puts "Thank you, you name is #{@player_1.name} and you game symbol is #{@player_1.game_symbol}.  Good luck beating #{@player_2.name}!  Hal's game symbol is #{@player_2.game_symbol}"
+  end
+
+  def computer_vs_computer
+  end
 
   def coin_toss
     puts "#{@player_1.name}, heads or tails?"
@@ -74,191 +95,29 @@ class Game
       coin = rand(1..2)
       call = 1
       if call == coin
+        @player_1.go_first = true
         puts "#{@player_1.name} won the toss. Please select your space."
       else
-        puts "#{@player_2.name} won the toss" 
+        @player_2.go_first = true
+        puts "#{@player_2.name} won the toss.  #{player_2.name} make your move." 
         # eval_board
       end
     elsif call == "tails"
       coin = rand(1..2)
       call = 2
       if call == coin
+        @player_1.go_first = true
          puts "#{@player_1.name} won the toss. Please select your space."
       else
+        @player_2.go_first = true
         puts "#{@player_2.name} won the toss, get ready to loose...Dave" 
         # eval_board
       end
     else
       puts "Invalid answer"
       coin_toss
-    end
-       
+    end  
   end
-
-  def start_game
-    puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|\n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|\n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|\n"
-    until game_is_over(@board) || tie(@board)
-      get_human_spot
-      eval_board
-      puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|\n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|\n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|\n"
-    end
-    puts "Game over"
-  end
-  # def get_human_spot
-  #   spot = nil
-  #   open_spaces = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-  #   until spot
-  #     spot = gets.chomp
-  #     number = spot.to_i
-  #     if @turns.include?(spot)
-  #       @turns.delete(spot)
-  #       @board[number] = @human
-  #       open_spaces.delete(number)
-  #       return spot = nil
-  #     else
-  #       puts "Please enter a valid open space on the board!"
-  #       get_human_spot
-  #     end
-  #   end
-  # end
-
-  # def eval_board
-  #   spot = nil
-  #   my_move = spot
-  #   until spot
-  #     if @board[4] == "X" && @board[0] == "O" || @board[4] == "O" && @board[0] == "X" || @board[4] == "X" && @board[0] == "X"
-  #       spot = get_best_move(@board, @computer)
-  #       if @board[spot] != "X" && @board[spot] != "O"
-  #         my_move = spot
-  #         @board[spot] = @computer
-  #       end
-  #     elsif @board[4] == "X" && @board[0] == "X"
-  #       if @board[8] == "8"
-  #         spot = 8
-  #         my_move = spot
-  #         @board[spot] = @computer
-  #         # puts "I win!"
-  #       elsif @board[8] == "O"
-  #         spot = checkmate(@board)
-  #         @board[spot] = @computer
-  #         puts "That's checkmate bruh!"
-  #       end
-  #     elsif @board[4] == "X"
-  #       if second_move(@board)
-  #         spot = second_move(@board)
-  #         my_move = spot
-  #         @board[spot] = @computer
-  #       else
-  #         spot = 0
-  #         my_move = spot
-  #         @board[spot] = "X"
-  #       end        
-  #     elsif @board[4] == "4"
-  #       spot = 4
-  #       my_move = spot
-  #       @board[spot] = @computer 
-  #     else
-  #       spot = nil
-  #     end 
-  #     # binding.pry
-  #     puts "My move was to space #{my_move}. Your turn!"    
-  #   end
-  # end
-
-  # def computer_went_first(board)
-  #   eight = [2, 6]
-  #   two = [0, 8]
-  #   zero = [2, 8]
-  #   if board[4] == "X" && board.count("X") == 1 && board.count("O") == 1
-  #     if board[8] == "O"
-  #       return eight.sample
-  #     elsif board[2] == "O"
-  #       return two.sample
-  #     elsif board[0] == "O"
-  #       return zero.sample
-  #     end
-  #   else
-
-  #   end
-  # end
-  # def second_move(board)
-  #   column_3 = [board[2], board[5], board[8]]
-  #   row_3 = [board[6], board[7], board[8]]
-  #   row_2 = [board[0], board[1], board[2]]
-  #   # best_move = nil
-
-  #   if column_3.count("O") == 2 || row_3.count("O") == 2
-  #     return get_best_move(@board, @computer)
-  #   end
-  # end
-
-  # def checkmate(board)
-  #   column_1 = [board[0], board[3], board[6]]
-  #   column_2 = [board[1], board[4], board[7]]
-  #   row_1 = [board[0], board[1], board[2]]
-  #   row_2 = [board[0], board[1], board[2]]
-  #   diagonal_1 = [board[2], board[4], board[6]]
-  #   best_move = nil
-
-  #   if column_1.include?("1") && column_1.include?("2") && row_2.include?("3") && row_2.include?("5")
-  #     best_move = board[3].to_i
-  #   elsif column_1.include?("1") && column_1.include?("2") && diagonal_1.include?("2") && row_2.include?("6")
-  #     best_move = board[6]
-  #   end
-  #   if best_move
-  #     return best_move
-  #   end
-  # end
-
-  # def get_best_move(board, next_player, depth = 0, best_score = {})
-  #   open_spaces = []
-  #   best_move = nil
-  #   board.each do |space|
-  #     if space != "X" && space != "O"
-  #       open_spaces << space
-  #     end
-  #   end
-  #   open_spaces.each do |open_space|
-  #     board[open_space.to_i] = @computer
-  #     if game_is_over(board)
-  #       best_move = open_space.to_i
-  #       board[open_space.to_i] = open_space
-  #       return best_move
-  #     else
-  #       board[open_space.to_i] = @human
-  #       if game_is_over(board)
-  #         best_move = open_space.to_i
-  #         board[open_space.to_i] = open_space
-  #         return best_move
-  #       else
-  #         board[open_space.to_i] = open_space
-  #       end
-  #     end
-  #   end
-  #   if best_move
-  #     return best_move
-  #   else
-  #     n = rand(0..open_spaces.count)
-  #     return open_spaces[n].to_i
-  #   end
-  # end
-
-  def game_is_over(board)
-
-    [board[0], board[1], board[2]].uniq.length == 1 ||
-    [board[3], board[4], board[5]].uniq.length == 1 ||
-    [board[6], board[7], board[8]].uniq.length == 1 ||
-    [board[0], board[3], board[6]].uniq.length == 1 ||
-    [board[1], board[4], board[7]].uniq.length == 1 ||
-    [board[2], board[5], board[8]].uniq.length == 1 ||
-    [board[0], board[4], board[8]].uniq.length == 1 ||
-    [board[2], board[4], board[6]].uniq.length == 1
-  end
-
-  def tie(board)
-    board.all? { |space| space == "X" || space == "O" }
-  end
-
 end
 
 game = Game.new
