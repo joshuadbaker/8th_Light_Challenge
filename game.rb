0526db0661c6fcd1a4dec4299e503faa4a6d8e5f
joshuadbaker <jbakercoder@gmail.com>
2015-11-11 10:36:27 -0500

    built out console through coin_toss method

--- game.rb
+++ game.rb
@@ -1,3 +1,12 @@
+# Notes from Jacob
+# create console file separate puts to that file
+# set gameboard[0] to an id
+# move turn logic back to game file
+# refactor into a single take_turn method apply it to each player/remove conditions from method
+# assess whether I need the turns array
+# refactor computer logic to a more basic rule for "best move"
+# move tie into game_is_over method as a condition
+# move versions into select_version
 require 'pry'
 require './human.rb'
 require './computer.rb'
@@ -12,113 +21,52 @@ class Game
 
   def create_new_game
     @new_gameboard = Gameboard.new()
-    puts "Hello..."
-    # sleep(2)
-    puts "...Dave"
-    # sleep(1)
-    puts "Welcome to Tic Tac Toe!"
-    # sleep(1)  
-    puts "Select from the following options and only type the first letter not the whole word\n(s)tart game\n(q)uit Tic Tac Toe"
-    response = gets.chomp
-    response = response.downcase
-    if response == "s" || response == "q"
-      until response == "q"
-        if response == "s"
-          select_version     
-        end
-        coin_toss
-        @new_gameboard.take_turn(@new_gameboard.board, @new_gameboard.turns, @player_1, @player_2) 
-      end
-    else 
-      puts "Please enter a valid command with the first letter."
-      sleep(1)
-      create_new_game
-    end
-  end
-
-  def select_version
-    puts "Select number for version of game:\n 1. Person vs. Person\n 2. Person vs. Computer\n 3. Computer vs. Computer"
-    game_version = gets.chomp
-    game_version = game_version.to_i
-    if game_version == 1
-      puts "You chose person vs person!"
-      person_vs_person
-    elsif game_version == 2
-      puts "You chose person vs computer, good luck against Hal!"
-      person_vs_computer
-    elsif game_version == 3
-      puts "Hal vs Skynet"
-    else 
-      abort("Thank you for playing!")
-    end
-  end
-
-  def person_vs_person
-    puts "Please enter the name for player 1."
-    name_1 = gets.chomp
-    puts 'Please enter a single character as your game symbol(The game will default to "X")'
-    # binding.pry
-    symbol_1 = gets.chomp
-    @player_1 = Human.new(name_1, symbol_1)
-    sleep(1)
-    puts "Thank you, your name is #{@player_1.name} and your game symbol is #{@player_1.game_symbol}"
+    # select(game_version)
+    # coin_toss
     
-    puts "Please enter the name for player 2."
-    name_2 = gets.chomp
-    puts 'Please enter a single character as your game symbol(The game will default to "X")'
-    symbol_2 = gets.chomp
-    @player_2 = Human.new(name_2, symbol_2)
-    sleep(1)
-    puts "Thank you, your name is #{@player_2.name} and your game symbol is #{@player_2.game_symbol}"
   end
 
-  def person_vs_computer
-    puts "Please enter the name for player 1."
-    name_1 = gets.chomp
-    puts 'Please enter a single character as your game symbol(The game will default to "X")'
-    symbol_1 = gets.chomp
-    @player_1 = Human.new(name_1, symbol_1)
-    # create_person(@player_1, name_1, symbol_1)
-    sleep(1)
-    @player_2 = Computer.new(name, game_symbol)
-    puts "Thank you, you name is #{@player_1.name} and you game symbol is #{@player_1.game_symbol}.  Good luck beating #{@player_2.name}!  Hal's game symbol is #{@player_2.game_symbol}"
-  end
-
-  def computer_vs_computer
+  def select(selection_number)
+    # binding.pry
+    if selection_number == 1
+     @player_1 = Human.new()
+     @player_2 = Human.new()
+    elsif selection_number == 2
+      @player_1 = Human.new()
+      @player_2 = Computer.new()
+    else
+      @player_1 = Computer.new()
+      @player_2 = Computer.new()
+    end
   end
 
   def coin_toss
-    puts "#{@player_1.name}, heads or tails?"
-    call = gets.chomp
-    call.downcase
-    if call == "heads"
-      coin = rand(1..2)
-      call = 1
-      if call == coin
-        @player_1.go_first = true
-        puts "#{@player_1.name} won the toss. Please select your space."
-      else
-        @player_2.go_first = true
-        puts "#{@player_2.name} won the toss.  #{player_2.name} make your move." 
-        # eval_board
-      end
-    elsif call == "tails"
-      coin = rand(1..2)
-      call = 2
-      if call == coin
-        @player_1.go_first = true
-         puts "#{@player_1.name} won the toss. Please select your space."
-      else
-        @player_2.go_first = true
-        puts "#{@player_2.name} won the toss, get ready to loose...Dave" 
-        # eval_board
-      end
+    if @player_1.go_first == true
+      @player_1.go_first = true
     else
-      puts "Invalid answer"
-      coin_toss
+      @player_2.go_first = true
     end  
+  end
+
+  def take_turn(board, turns, player)
+    spot = nil
+    # open_spaces = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    until spot
+      spot = gets.chomp
+      number = spot.to_i
+      if turns.include?(spot)
+        turns.delete(spot)
+        board[number] = player.id 
+        # open_spaces.delete(number)
+        # binding.pry    
+        take_turn(player)
+        spot = nil
+      else
+        puts "Please enter a valid open space on the board!"
+        take_turn(board, turns, player_1, player_2)
+      end
+    end
   end
 end
 
-game = Game.new
-game.create_new_game
+
